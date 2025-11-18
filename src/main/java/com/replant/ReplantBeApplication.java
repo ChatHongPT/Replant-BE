bbc8b6103620e561a4182e9dc1c5da0f0d6256c0
replant1 <replant1>
2025-11-19 03:32:29 +0900

    intial

---
+++ src/main/java/com/replant/ReplantBeApplication.java
@@ -0,0 +1,13 @@
+package com.replant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplantBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplantBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/replant/ReplantBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplantBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
